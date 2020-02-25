2c8f934f8bed07e22d07c48e7239120e729b3fe1
Silje Heldal Monsen <Silje24@yahoo.com>
2020-02-25 14:59:42 +0100

    implemented javascript on all the pages

--- javascript/script.js
+++ javascript/script.js
@@ -17,3 +17,15 @@ window.onclick = function (e) {
     }
 	}
 }
+
+
+// Checkout button
+
+//Click event
+var myBtn = document.querySelector('button');
+
+function myAlert () {
+     alert("You will now proceed to checkout");
+}
+
+myBtn.addEventListener('click', myAlert);
